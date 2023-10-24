e5455e0b2c2a519ca978258e9c14b2a13471368b
rishabh choubey <rishabhchaubey13@gmail.com>
2023-10-24 17:23:34 +0530

    dockerized

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         msg_light:
           "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
         shadow_rght:
-          " rgba(136, 165, 191, 0.48) 0px 1px 8px 0px, rgba(255, 255, 255, 0.8) 0px -1px 8px 0px;",
+          " rgba(136, 165, 191, 0.48) 0px 1px 8px 0px, rgba(255, 255, 255, 0.8) 0px -1px 8px 0px",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
